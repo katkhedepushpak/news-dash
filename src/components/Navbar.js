763b04565f25b0d6780d16f88cb351c24333337f
Pushpak Katkhede <katkhedepushpak@gmail.com>
2024-12-07 23:43:02 -0800

    Modified the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './css/Navbar.css';
+import React, { Component } from "react";
+import "./css/Navbar.css";
 
 export class Navbar extends Component {
+  handleCountrySelect = (countryCode) => {
+    this.props.onCountryChange(countryCode);
+  };
+
   render() {
+    const countryCodes = [
+      'ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'
+    ];
+
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
-              
               <img src="/img/logo-1.png" alt="Logo" />
             </a>
             <button
@@ -29,8 +36,47 @@ export class Navbar extends Component {
                     Home
                   </a>
                 </li>
-
               </ul>
+              <div className="d-flex ms-auto small-nav-size">
+                <ul className="navbar-nav">
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Country
+                    </a>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                      {countryCodes.map((code) => (
+                        <li key={code}>
+                          <a
+                            className="dropdown-item"
+                            href="#"
+                            onClick={() => this.handleCountrySelect(code)}
+                          >
+                            {code.toUpperCase()}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+                <form className="d-flex ms-3" role="search">
+                  <input
+                    className="form-control me-2 small-nav-size"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-outline-light small-nav-size" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </nav>
